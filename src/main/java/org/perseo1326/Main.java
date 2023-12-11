30838d84dba4c4ff3376121926d4e4842f224b5d
John W <perseo1326@gmail.com>
2023-12-11 22:49:50 +0100

    login page and ask user info

--- src/main/java/org/perseo1326/Main.java
+++ src/main/java/org/perseo1326/Main.java
@@ -1,28 +1,76 @@
 package org.perseo1326;
-
+import java.io.Console;
 import java.io.IOException;
 
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.Page;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+
 public class Main {
+
+    private static String readUserInput(String message){
+
+        Console console = System.console();
+        String str = console.readLine(message);
+        if(str.equals("")){
+            System.out.println("Debe ingresar un " + message);
+            readUserInput(message);
+        }
+        return str;
+    }
+
+    
+    private static Page loginProcess(HtmlElement htmlElement, String userId, String pass) throws IOException{
+
+            HtmlElement loginForm = htmlElement.getOneHtmlElementByAttribute("form", "name", "bsUserForm");
+
+            // System.out.println(loginForm);
+
+            HtmlElement inputUserId = loginForm.getOneHtmlElementByAttribute("input", "id", "UserId");
+            inputUserId.setAttribute("value", userId);
+
+            // System.out.println(inputUserId);
+
+            HtmlElement inputPass = loginForm.getOneHtmlElementByAttribute("input", "id", "UserPassword");
+            inputPass.setAttribute("value", pass);
+
+            // System.out.println(inputPass);
+
+            HtmlElement inputSubmit = loginForm.getOneHtmlElementByAttribute("input", "name", "loginButton");
+            return inputSubmit.click();
+
+    }
+
     public static void main(String[] args) {
 
-//        final String URL = "https://info.cern.ch/hypertext/WWW/TheProject.html";
         // final String URL = "https://www.baeldung.com/htmlunit";
-        final String URL = "https://www.devhut.net/vba-creating-an-html-document/";
+        // final String URL = "https://www.devhut.net/vba-creating-an-html-document/";
+        String userId = "";
+        String pass = ""; 
+        final String LOGIN_URL = "http://servidor.com/dispatch/";
 
         System.out.print("\033[H\033[2J");
         System.out.flush();
 
         try {
-            WebScrapper webScrapper = new WebScrapper(URL);
 
-            webScrapper.extractInformation();
-            System.out.println("\n");
+            userId = readUserInput("Nombre de usuario: ");
+            pass = readUserInput("Contraseña: ");
+            WebScrapper webScrapper = new WebScrapper(LOGIN_URL);
 
-            webScrapper.findByElement();
+            Page pagina = loginProcess(webScrapper.getPage().getBody(), userId, pass);
+            System.out.println("GET WEB RESPONSE: \n" + pagina.getWebResponse().getContentAsString());
+            // System.out.println(pagina.toString());
 
 
         } catch (IOException e) {
             throw new RuntimeException(e);
+        } catch (FailingHttpStatusCodeException e){
+            System.out.println(e.getLocalizedMessage());
+        } catch (ElementNotFoundException e) {
+            System.out.println("EXCEPCION: Elemento NO encontrado...");
+            System.out.println(e.getMessage() + "\n");
         }
 
 
--- src/main/java/org/perseo1326/WebScrapper.java
+++ src/main/java/org/perseo1326/WebScrapper.java
@@ -2,8 +2,8 @@ package org.perseo1326;
 
 
 import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.DomElement;
 import com.gargoylesoftware.htmlunit.html.DomNodeList;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlInput;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -30,49 +30,83 @@ public class WebScrapper {
         return webClient.getPage(url);
     }
 
-    public void extractInformation() throws IOException{
-        
-        HtmlForm searchForm = null;
-        HtmlInput submitButton;
-
-        List<HtmlForm> items = this.page.getByXPath("//form");
-
-        for( HtmlForm element : items ) {
-            if(element.getAttribute("role").equals("search")){
-                System.out.println(element.toString());
-                searchForm = element; 
-            }
-        }
-                
-        HtmlTextInput textNode = searchForm.getInputByName("s");
-        textNode.type("HTML");
-
-        System.out.println(textNode.toString());
-
-        submitButton = searchForm.getInputByValue("Search");
-        this.page = submitButton.click();
+    public HtmlPage getPage(){
+        return this.page;
     }
 
-    public void findByElement() {
+    // public DomNodeList<HtmlElement> findElements(HtmlElement htmlElement, String tagName) {
 
-        DomNodeList<DomElement> articles = this.page.getElementsByTagName("article");
+    //     return htmlElement.getElementsByTagName(tagName);
+    // }
 
-        for( DomElement article : articles) {
+    // public HtmlElement findElementByNameFromArray(DomNodeList <HtmlElement> domNodeList, String attributeName, String attributeValue){
 
-            List<DomElement> elements = article.getByXPath("header/h1");
+    //     for (HtmlElement element : domNodeList) {
+                
+    //         if(element.getAttribute(attributeName).equals(attributeValue)){
+    //             System.out.println("Elemento: \n" + element + "\nAtributo: " + attributeName + ": " + attributeValue + "\n");
+    //             return element;
+    //         }
+    //     }
 
-            // System.out.println("ELEMENTOS DEL ARTICLE: ");
-            for(DomElement h1 : elements ){
+    //     return null;
+    // }
 
-                System.out.println(h1.getTextContent() );
-            }
 
-        }
+    // public void findFormByAttributeValue(HtmlElement htmlElement, String attributeName, String attributeValue ){
+    //     var x = htmlElement.getOneHtmlElementByAttribute("form", attributeName, attributeValue)(null);
+    //     System.out.println(x);
+    // }
+
+
+
+
+
+
+    
+    // public void extractInformation() throws IOException{
+        
+    //     HtmlForm searchForm = null;
+    //     HtmlInput submitButton;
+
+    //     List<HtmlForm> items = this.page.getByXPath("//form");
+
+    //     for( HtmlForm element : items ) {
+    //         if(element.getAttribute("role").equals("search")){
+    //             System.out.println(element.toString());
+    //             searchForm = element; 
+    //         }
+    //     }
+                
+    //     HtmlTextInput textNode = searchForm.getInputByName("s");
+    //     textNode.type("HTML");
+
+    //     System.out.println(textNode.toString());
+
+    //     submitButton = searchForm.getInputByValue("Search");
+    //     this.page = submitButton.click();
+    // }
+
+    // public void findByElement() {
+
+    //     DomNodeList<DomElement> articles = this.page.getElementsByTagName("article");
+
+    //     for( DomElement article : articles) {
+
+    //         List<DomElement> elements = article.getByXPath("header/h1");
+
+    //         // System.out.println("ELEMENTOS DEL ARTICLE: ");
+    //         for(DomElement h1 : elements ){
+
+    //             System.out.println(h1.getTextContent() );
+    //         }
+
+    //     }
 
         // for( DomNode nodo : nodosDom ){
         //     System.out.println(nodo.toString());
         // }
 
 
-    }
+    // }
 }
